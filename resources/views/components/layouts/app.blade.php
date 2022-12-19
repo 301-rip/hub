55e5179d317853a3bf964d8d6c004ee2cad0aeef
Chris Morrell <inxilpro@users.noreply.github.com>
2022-12-19 12:02:39 -0500

    WIP

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,23 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>@yield('pageTitle') - 301 R.I.P.</title>
-        
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-
     <body class="m-0 p-0 bg-slate-700 flex flex-col min-h-screen  items-center justify-center">
         <div class="flex flex-col items-center justify-center">
             @svg('mammoth2', 'h-48')
             <div class="w-90% max-w-xl relative my-12 before:absolute before:left-4 before:top-4 before:w-full before:h-full before:z-20 before:rounded-lg before:bg-gradient-to-tl before:from-cyan-400 before:to-indigo-500">
-                <div class="relative shadow bg-slate-100 rounded-lg shadow p-8 border-black z-30">
+                <div {{ $attributes->class('relative shadow bg-slate-100 rounded-lg shadow px-8 py-8 border-black z-30') }}>
                     {{ $slot ?? '' }}
                 </div>
             </div>
         </div>
         @stack('js')
-        @routes
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,40 @@
 <x-layouts.app>
-    <div x-data="url">
+    <form x-data="url" @submit.prevent="submit()">
         <label class="sr-only" for="url">Mastadon Link to Obfuscate</label>
-        <div class="flex">
-            <input class="flex-grow border-t border-l border-b border-slate-300 p-2 rounded-l" placeholder="https://hachyderm.io/@301RIP" id="url" name="url" type="text" x-model="url" />
-            <button class="px-4 py-2 bg-gradient-to-tl from-cyan-600 to-indigo-500 hover:from-cyan-400 hover:to-indigo-400 rounded-r text-white font-bold" @click="submit">Rip it!</button>
+        <div class="mt-1 flex rounded-md shadow-sm">
+            <div class="relative flex flex-grow items-stretch focus-within:z-10">
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <x-heroicon-s-link class="h-5 w-5 text-gray-400"/>
+                </div>
+                <input
+                    class="block w-full rounded-none rounded-l-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    pattern="([Hh][Tt]{2}[Pp][Ss]?:\/\/)?([\w\-]+\.)+([a-zA-Z]{2,63})(\/.*)?"
+                    placeholder="https://hachyderm.io/@301RIP"
+                    id="url"
+                    name="url"
+                    type="text"
+                    x-model="url"
+                    autofocus
+                    required
+                />
+            </div>
+            <button
+                type="submit"
+                class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-gradient-to-tl from-cyan-600 to-indigo-500 text-white px-4 py-2 text-sm font-medium hover:from-cyan-400 hover:to-indigo-400 focus:border-indigo-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:from-cyan-200 focus:to-indigo-300 disabled:from-gray-200 disabled:to-gray-100"
+                :disabled="!valid()"
+            >
+                <span class="sr-only">Submit</span>
+                <x-heroicon-s-arrow-right class="w-5 h-5" />
+            </button>
         </div>
-    </div>
+        <div class="hidden m-1 text-sm font-semibold text-red-600" :class="{ hidden: valid() || '' === url }" x-text="error()"></div>
+    </form>
 
     <p class="mt-8 text-slate-500 text-sm">
-        <a href="https://301.rip" class="border-dotted border-b border-black">301.rip</a> is a utility site to uncensor links to Mastadon. Paste a link to a Mastadon instance, user, or post above to get started.
+        <a href="https://301.rip" class="border-dotted border-b border-black">301.rip</a> is a utility site to uncensor
+        links to Mastadon. Paste a link to a Mastadon instance, user, or post above to get started. Built by
+        <a href="https://hachyderm.io/@coulb" class="font-semibold hover:underline" rel="me" target="_blank">Daniel</a>
+        and <a href="https://jawns.club/@inxilpro" class="font-semibold hover:underline" rel="me" target="_blank">Chris</a>.
     </p>
 
     @once
@@ -16,8 +42,16 @@
             <script>
                 document.addEventListener('alpine:init', () => {
                     Alpine.data('url', () => ({
-                        url: null,
-
+                        url: '',
+                        init() {
+                            this.valid();
+                        },
+                        valid() {
+                            return `${this.url}`.match(/^([Hh][Tt]{2}[Pp][Ss]?:\/\/)?([\w\-]+\.)+([a-zA-Z]{2,63})(\/.*)?/);
+                        },
+                        error() {
+                            return `Please enter a valid URL.`;
+                        },
                         submit() {
                             window.location.href = `/${this.url}`;
                         }
