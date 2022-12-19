6813a1888f2a6dfac9094306df8f4c074813a312
Chris Morrell <inxilpro@users.noreply.github.com>
2022-12-18 23:43:02 -0500

    Spaces

--- app/Http/Controllers/RedirectsController.php
+++ app/Http/Controllers/RedirectsController.php
@@ -13,23 +13,23 @@ class RedirectsController extends Controller
 {
     public function create($url)
     {
-		return view('redirects.create', [
-			'original' => $url,
-			'rip' => (string) new RedirectUrl($url),
-		]);
+        return view('redirects.create', [
+            'original' => $url,
+            'rip' => (string) new RedirectUrl($url),
+        ]);
     }
-	
-	public function show(KnownInstance $instance, $path)
-	{
-		return view('redirects.show', [
-			'url' => $this->obfuscate($instance->url($path)),
-		]);
-	}
-	
-	protected function obfuscate(string $url): Js
-	{
-		$chunks = str_split(base64_encode($url), random_int(5, 10));
-		
-		return Js::from($chunks);
-	}
+
+    public function show(KnownInstance $instance, $path)
+    {
+        return view('redirects.show', [
+            'url' => $this->obfuscate($instance->url($path)),
+        ]);
+    }
+
+    protected function obfuscate(string $url): Js
+    {
+        $chunks = str_split(base64_encode($url), random_int(5, 10));
+
+        return Js::from($chunks);
+    }
 }
